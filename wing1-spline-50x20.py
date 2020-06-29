7668126f7b1ebbf65619dc847763bfc891b68327
Wink Saville <wink@saville.com>
2020-06-29 15:00:09 -0700

    Changed wing1-spline to print 50x20 and renamed it
    It is now wing1-spline-50x20.py.

--- wing1-spline-50x20.py
+++ wing1-spline-50x20.py
@@ -1,5 +1,5 @@
 import sys
-log(sys.path)
+log(f'sys.path={sys.path}')
 
 from mh49 import mh49
 from scale import scaleListOfTuple
@@ -10,7 +10,7 @@ import cadquery as cq # type: ignore
 from typing import Tuple, List
 
 chord: float = 50
-span: float = 50
+span: float = 20
 
 # Normalize mh49 to 1. Assumes first point is
 # the length of the chord and will scale all
@@ -29,6 +29,7 @@ fMh49: List[Tuple[float, float]] = fattenTe(sMh49, 0.5, 10)
 result = cq.Workplane("XY").spline(fMh49).close().extrude(span)
 #pprint(vars(result))
 import io
-f = io.open('wing1-spline-direct-0.5.stl', 'w+')
-cq.exporters.exportShape(result, cq.exporters.ExportTypes.STL, f, 0.5)
+tolerance=0.001;
+f = io.open(f'wing1-spline-50x20-direct-{tolerance}.stl', 'w+')
+cq.exporters.exportShape(result, cq.exporters.ExportTypes.STL, f, tolerance)
 f.close()
