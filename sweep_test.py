ad79c4bfc468f5eda96bd8d255bfbfa5b8cbb8ba
Wink Saville <wink@saville.com>
2020-06-30 13:29:23 -0700

    Make the bend vertical

--- sweep_test.py
+++ sweep_test.py
@@ -8,6 +8,10 @@ s = (
     .ellipse(a,b)
     .sweep(
         cq.Workplane("XZ")
-        .spline([(0,0),(0,h),(d,h+5*d)],[(0,1),(1,1)])
+        # Start 0deg tangent, Tip finishes with 90deg tangent
+        .spline([(0, i) for i in range(h)] + [(d,h)],[(0,1),(1,0)])
+
+        # Start 0deg tangent, Tip finishes with 45deg tangent
+        #.spline([(0,0),(0,h),(d,h+5*d)],[(0,1),(1,1)]) #original
     )
 )
